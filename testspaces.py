a335647e73802019d9950b48b5b1561cd08fd49a
luludarcy <luludarcy@gmail.com>
2018-11-16 13:28:24 +0000

    need to change adjacency array type to int to ensure a usable discrete state

--- testspaces.py
+++ testspaces.py
@@ -1,5 +1,6 @@
 import numpy as np
 from gym import spaces
+from graph_tool.all import *
 
 md = spaces.MultiDiscrete([ 5, 2, 2 ])
 for i in range(10):
@@ -11,5 +12,16 @@ print(md.contains(np.array([-1, 0, 0], dtype=np.int32)))
 new = spaces.MultiDiscrete(np.full((10,10),2))
 print(np.asarray(np.full((10,10),2)))
 print(new.sample())
+
+g = Graph()
+g.set_fast_edge_removal(True)
+number_input_nodes = 3
+total_services = 10
+inputServices = g.add_vertex(number_input_nodes)
+otherServices = g.add_vertex(total_services - number_input_nodes)
+adj = adjacency(g).toarray().astype(int)
+print(adj)
+print(new.sample())
+print(new.contains(adj))
 #for i in range(10):
 #    print(new.sample())
